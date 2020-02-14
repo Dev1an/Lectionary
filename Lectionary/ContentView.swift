388bfffbb6fb54f42e01faab15390b7513c0d19e
Damiaan Dufaux <damiaan@dpro.be>
2020-02-14 14:35:43 +0100

    Initial Commit

---
+++ Lectionary/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Lectionary
+//
+//  Created by Damiaan on 14/02/2020.
+//  Copyright © 2020 Devian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
